9ae0cd17360072ca9a23ac08a62d65bf00ec2198
Kian Hooshmand <kianhooshmand@MacBook-Pro-8.local>
2022-07-05 17:39:44 -0400

    map rendering w/ custom style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
+import { formatRelative } from "date-fns";
+import mapStyles from "./mapStyles";
+
+
+const libraries = ["places"];
+const mapContainerStyle = {
+  height: "100vh",
+  width: "100vw",
+};
+
+const center = {
+  lat: 40.7831,
+  lng: -73.9712,
+};
+
+const options = {
+  styles: mapStyles,
+  disableDefaultUI: true,
+}
 
 function App() {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries,
+  });
+
+  if (loadError) return "Error";
+  if (!isLoaded) return "Loading...";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Olympus
+      </h1>
+
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={12}
+        center={center}
+        options={options}
+      ></GoogleMap>
     </div>
   );
 }
