767fc8d1f444a9d2e6bc34d7300d68503a756378
Kian Hooshmand <kianhooshmand@MacBook-Pro-8.local>
2022-07-06 00:19:43 -0400

    update file structure and add linting

--- src/App.js
+++ src/App.js
@@ -1,95 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 import React from "react";
 import { WalletLinkConnector } from "@web3-react/walletlink-connector";
 import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
 import { InjectedConnector } from "@web3-react/injected-connector";
-import { useWeb3React } from '@web3-react/core';
+import { useWeb3React } from "@web3-react/core";
 
-import {
-  GoogleMap,
-  useLoadScript,
-  Marker,
-  InfoWindow,
-} from "@react-google-maps/api";
-import usePlacesAutocomplete, {
-  getGeocode,
-  getLatLng,
-} from "use-places-autocomplete";
-import {
-  Combobox,
-  ComboboxInput,
-  ComboboxPopover,
-  ComboboxList,
-  ComboboxOption,
-} from "@reach/combobox";
-import { formatRelative } from "date-fns";
-import mapStyles from "./mapStyles";
-
-
-const libraries = ["places"];
-const mapContainerStyle = {
-  height: "100vh",
-  width: "100vw",
-};
-
-const center = {
-  lat: 40.7831,
-  lng: -73.9712,
-};
-
-const options = {
-  styles: mapStyles,
-  disableDefaultUI: true,
-}
+import Map from "./components/Map";
 
 const CoinbaseWallet = new WalletLinkConnector({
   url: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
   appName: "Web3-react Demo",
   supportedChainIds: [1, 3, 4, 5, 42],
- });  
+});
 
- const WalletConnect = new WalletConnectConnector({
+const WalletConnect = new WalletConnectConnector({
   rpcUrl: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
   bridge: "https://bridge.walletconnect.org",
   qrcode: true,
- });
+});
 
- const Injected = new InjectedConnector({
-  supportedChainIds: [1, 3, 4, 5, 42]
- });
+const Injected = new InjectedConnector({
+  supportedChainIds: [1, 3, 4, 5, 42],
+});
 
 function App() {
   const { activate, deactivate } = useWeb3React();
-  const { active, chainId, account } = useWeb3React();
-  const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-    libraries,
-
-  });
-
-  if (loadError) return "Error";
-  if (!isLoaded) return "Loading...";
+  const { account } = useWeb3React();
 
   return (
     <div className="App">
       <h1>
         Olympus
-        <button onClick={() => { activate(CoinbaseWallet) }}>Coinbase Wallet</button>
-        <button onClick={() => { activate(WalletConnect) }}>Wallet Connect</button>
-        <button onClick={() => { activate(Injected) }}>Metamask</button>
+        <button
+          onClick={() => {
+            activate(CoinbaseWallet);
+          }}
+        >
+          Coinbase Wallet
+        </button>
+        <button
+          onClick={() => {
+            activate(WalletConnect);
+          }}
+        >
+          Wallet Connect
+        </button>
+        <button
+          onClick={() => {
+            activate(Injected);
+          }}
+        >
+          Metamask
+        </button>
         <button onClick={deactivate}>Disconnect</button>
         <div>Wallet: {account}</div>
-      
       </h1>
-    
-     
-      <GoogleMap
-        mapContainerStyle={mapContainerStyle}
-        zoom={12}
-        center={center}
-        options={options}
-      ></GoogleMap>
+
+      <Map />
     </div>
   );
 }
---
+++ src/components/Map.js
@@ -0,0 +1,78 @@
+import React from "react";
+import mapStyles from "./mapStyles";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+
+const libraries = ["places"];
+const mapContainerStyle = {
+  height: "100vh",
+  width: "100vw",
+};
+
+const center = {
+  lat: 40.7831,
+  lng: -73.9712,
+};
+
+const options = {
+  styles: mapStyles,
+  disableDefaultUI: true,
+};
+
+const Map = () => {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries,
+  });
+
+  const [markers, setMarkers] = React.useState([]);
+  const [setSelected] = React.useState(null);
+
+  const mapRef = React.useRef();
+  const onMapLoad = React.useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+
+  const onMapClick = React.useCallback((event) => {
+    setMarkers((current) => [
+      ...current,
+      {
+        lat: event.latLng.lat(),
+        lng: event.latLng.lng(),
+        time: new Date(),
+      },
+    ]);
+  }, []);
+
+  if (isLoaded) {
+    return (
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={12}
+        center={center}
+        options={options}
+        onClick={onMapClick}
+        onLoad={onMapLoad}
+      >
+        {markers.map((marker) => (
+          <Marker
+            key={marker.time.toISOString()}
+            position={{ lat: marker.lat, lng: marker.lng }}
+            icon={{
+              url: "https://upload.wikimedia.org/wikipedia/commons/4/46/Bitcoin.svg",
+              scaledSize: new window.google.maps.Size(30, 30),
+              origin: new window.google.maps.Point(0, 0),
+              anchor: new window.google.maps.Point(15, 15),
+            }}
+            onClick={() => {
+              setSelected(marker);
+            }}
+          />
+        ))}
+      </GoogleMap>
+    );
+  } else {
+    return null;
+  }
+};
+
+export default Map;
